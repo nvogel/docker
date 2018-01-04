141ced3c2f7ea38c986e75edd6dab84f9365841b
Nicolas Vogel <nvogel.dvp@gmail.com>
2018-01-04 11:56:26 +0100

    Switching to Flask

--- net/files/app/app.py
+++ net/files/app/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-  return 'Flask Dockerized'
+  return 'Hello world'
+
+@app.route('/headers')
+def headers():
+  return "{}".format(request.headers)
+
+@app.route('/ip')
+def ip():
+  app.logger.debug("Request remote addr %s",  request.remote_addr)
+  app.logger.debug("Request X-Forwarded-For %s", request.headers.get('X-Forwarded-For', 'empty'))
+  return request.headers.get('X-Forwarded-For', request.remote_addr)
 
 if __name__ == '__main__':
-  app.run(host='::', port=8080, debug=True)
-  #app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='::', port=8080)
